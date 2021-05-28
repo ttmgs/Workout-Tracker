d4b34bd6d466da5a8830d633da723eff94a322a3
Magdi Tiea <magditiea@gmail.com>
2021-05-28 09:55:31 -0400

    api routes added

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,1 +1,35 @@
-const db = require('')
+const db = require("../models");
+const express = require("express");
+const app = express();
+
+module.exports = function (app) {
+  // retreive all workouts
+  app.get("/api/workouts", (req, res) => {
+    db.Workout.find({}, (err, data) => {
+      if (err) throw err;
+      res.json(data);
+    });
+  });
+
+  // add a new exercise to the workout
+  app.post("/api/workouts/:id", (req, res) => {
+    db.Workout.findOneAndUpdate(
+      { _id: params.id },
+      { $push: { exercises: body } },
+      {
+        upsert: true,
+        useFindModify: false,
+      },
+      (updatedWorkout) => {
+        res.json(updatedWorkout);
+      }
+    );
+  });
+
+  // create a workout
+  app.post("/api/workouts", (req, res) => {
+    db.Workout.create({}).then((newWorkout) => {
+      res.json(newWorkout);
+    });
+  });
+};
